e6b7c13cbc1e05f17396db75c4b335b17c41897b
ushu3323 <redspower10@gmail.com>
2023-04-13 09:23:39 -0300

    Lint source code

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Nav from './Nav'
-import Footer from './Footer'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Footer from './Footer';
+import Nav from './Nav';
+
 
 
 export default function Layout() {
   return (
     <>
-        <Nav/>
-            <Outlet/>
-        <Footer/>
+      <Nav/>
+      <Outlet/>
+      <Footer/>
     </>
-  )
+  );
 }
